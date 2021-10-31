60ff1b165cb64eda49ff342005108c5405aeed6d
Arek001 <59519837+Arek001@users.noreply.github.com>
2021-10-31 20:30:00 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void usmiech (void)
+{
+    printf("usmiech!");
+}
+
+int main()
+{
+    usmiech();
+    usmiech();
+    usmiech();
+    printf("\n");
+    usmiech();
+    usmiech();
+    printf("\n");
+    usmiech();
+    return 0;
+}
